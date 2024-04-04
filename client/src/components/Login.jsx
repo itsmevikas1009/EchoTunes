f6f26de652270bef3434af99ce4d2d6f8a784579
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-04-04 19:42:50 +0530

    Login Signup Page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,46 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <div className="h-screen w-full ">
+      <div className=" hidden md:flex md:w-[50%]  flex-col items-center bg-[rgb(213,129,53)] h-full">
+        <img src="https://staticfe.saavn.com/web6/jioindw/dist/1712206575/_i/artist/Badshah.png" className="h-96 mt-[6rem]" alt="" />
+        <div className="text-center  text-white mt-10 ">
+          <p className="text-4xl font-semibold mb-2">All Your Music.</p>
+          <p className="text-xl font-semibold italic">Anytime, anywhere</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full  md:w-[50%] mx-auto absolute right-0 top-[7rem] p-[2.5rem] ">
+        <div className="w-11/12 md:w-9/12">
+          <h1 className="text-4xl font-bold ">Welcome to Spotify</h1>
+          <p className="text-xl p-1">Login with your email...</p>
+
+          <form className="flex flex-col gap-4 mt-12 " onSubmit={handleSubmit}>
+            <div className="w-full">
+              <label htmlFor="" className="block font-medium">Email</label>
+              <input type="email" placeholder="Email" className="w-full p-2 rounded-md outline-none  text-black border border-gray-200 px-4" />
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="" className="block font-medium">Password</label>
+              <input type="password" placeholder="Password" className="w-full p-2 rounded-md outline-none  text-black border border-gray-200 px-4" />
+            </div>
+
+
+            <button className="w-full bg-green-500 rounded-md p-2 mt-6 font-semibold text-lg">Login</button>
+          </form>
+
+          <p className="mt-6 text-lg ">New User ? <Link to="/signup" className="text-blue-500">SignUp</Link> </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,51 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <>
-      <h1>SignUp Page</h1>
-    </>
+    <div className="h-screen w-full ">
+      <div className=" hidden md:flex md:w-[50%]  flex-col items-center bg-[rgb(213,129,53)] h-full">
+        <img src="https://staticfe.saavn.com/web6/jioindw/dist/1712206575/_i/artist/Badshah.png" className="h-96 mt-[6rem]" alt="" />
+        <div className="text-center  text-white mt-10 ">
+          <p className="text-4xl font-semibold mb-2">All Your Music.</p>
+          <p className="text-xl font-semibold italic">Anytime, anywhere</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full  md:w-[50%] mx-auto absolute right-0 top-[7rem] p-[2.5rem] ">
+        <div className="w-11/12 md:w-9/12">
+          <h1 className="text-4xl font-bold ">Welcome to Spotify</h1>
+          <p className="text-xl p-1">Sign up with your details...</p>
+
+          <form className="flex flex-col gap-4 mt-12 " onSubmit={handleSubmit}>
+            <div className="">
+              <label htmlFor="" className="block font-medium">Name</label>
+              <input type="text" placeholder="Name" className="w-full p-2 rounded-md outline-none  text-black border border-gray-200 px-4" />
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="" className="block font-medium">Email</label>
+              <input type="email" placeholder="Email" className="w-full p-2 rounded-md outline-none  text-black border border-gray-200 px-4" />
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="" className="block font-medium">Password</label>
+              <input type="password" placeholder="Password" className="w-full p-2 rounded-md outline-none  text-black border border-gray-200 px-4" />
+            </div>
+
+
+            <button className="w-full bg-green-500 rounded-md p-2 mt-6 font-semibold text-lg">SignUp</button>
+          </form>
+
+          <p className="mt-6 text-lg">Already Have a Account ? <Link to="/login" className="text-blue-500">Login</Link> </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
