6a46a8225ae9c804e646e7f7ba316b621372584f
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2024-04-07 00:37:40 +0530

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,16 +6,17 @@ const Dashboard = () => {
   const [data, setData] = useState(null);
 
   const newRelaeses = async () => {
-    let response = await axios.get('https://v1.nocodeapi.com/amn123/spotify/dMGVLtPyXiNWgLWG/browse/new?country=india');
-    console.log(response.data.albums.items);
-    setData(response.data.albums.items);
-  }
+    let response = await axios.get(
+      "https://v1.nocodeapi.com/amn123/spotify/dMGVLtPyXiNWgLWG/browse/new?country=india"
+    );
+    if (response.status === true) {
+      setData(response.data.albums.items);
+    }
+  };
 
   useEffect(() => {
     newRelaeses();
-  }, [])
-
-
+  }, []);
 
   return (
     <AppLayout>
@@ -33,27 +34,28 @@ const Dashboard = () => {
           </div>
         </div>
 
-
         <h2 className="text-2xl font-bold mt-8">Trending now near you</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 py-8 gap-y-8">
-          {
-            data ? data.map((i, index) => (
-              <div key={index}>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          {data ? (
+            data.map((i, index) => (
+              <div
+                key={index}
+                className="bg-[#232323] rounded-lg  p-3 ms-4 mt-4"
+              >
                 <img
                   src={`${i.images[0].url}`}
                   alt=""
                   className="rounded-lg "
                 />
-                <p className="text-xl my-2 font-semibold">{i.name.length > 15 ? i.name.slice(0, 15) : i.name}</p>
+                <p className="text-xl my-2 font-semibold">
+                  {i.name.length > 15 ? i.name.slice(0, 15) : i.name}
+                </p>
                 <p className="text-sm">{i.artists[0].name}</p>
               </div>
-            )) : (
-              <h1>No Data</h1>
-            )
-          }
-
-
-
+            ))
+          ) : (
+            <h1>No Data</h1>
+          )}
         </div>
         <h2 className="text-2xl font-bold  mt-6">Featured Charts</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
