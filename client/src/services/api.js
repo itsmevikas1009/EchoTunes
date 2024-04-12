e32439ab145506cdbb70ef359cf762cd7753a653
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2024-04-13 00:22:31 +0530

    Vercel

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,5 @@
-export const server = "http://localhost:3000/api"
+const localUrl = "http://localhost:3000/api";
+
+const remoteURL = "https://spotify-project-server.vercel.app/api"
+
+export const server = remoteURL;
