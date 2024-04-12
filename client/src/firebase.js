c5e6bcd4b080499414fb748db53028fa662a2697
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-04-13 00:19:23 +0530

    bug

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,34 +1,9 @@
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getStorage } from "firebase/storage";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//     authDomain: "blog-26eb9.firebaseapp.com",
-//     projectId: "blog-26eb9",
-//     storageBucket: "blog-26eb9.appspot.com",
-//     messagingSenderId: "134761218625",
-//     appId: "1:134761218625:web:974b39697bdd36d11efcc0"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-// const storage = getStorage(app);
-// export default storage;
-
-
-
-
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDuPUNcKqUk94Gwr0xQV7KlP_BIoJKxX4I",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "spotify-92d44.firebaseapp.com",
     projectId: "spotify-92d44",
     storageBucket: "spotify-92d44.appspot.com",
